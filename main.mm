2c22598edc5c128350ad93f5afc2687d25508d75
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-02 05:08:28 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -1,14 +1,24 @@
-TWEAK_NAME = Moustache
+#import <UIKit/UIKit.h>
 
-Moustache_FILES = main.mm
-Moustache_FRAMEWORKS = UIKit Foundation
+__attribute__((constructor))
+static void init_tweak() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
 
-Moustache_CFLAGS = -fobjc-arc
+        UIWindow *window = UIApplication.sharedApplication.keyWindow;
+        if (!window) return;
 
-ARCHS = arm64 arm64e
-TARGET = iphone:clang:latest:14.0
+        UIView *box = [[UIView alloc] initWithFrame:CGRectMake(60, 200, 220, 120)];
+        box.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.8];
+        box.layer.cornerRadius = 12;
 
-INSTALL_TARGET_PROCESSES = SpringBoard
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 200, 40)];
+        label.text = @"Moustache dylib loaded 🔥";
+        label.textColor = UIColor.whiteColor;
+        label.font = [UIFont boldSystemFontOfSize:14];
 
-include $(THEOS)/makefiles/common.mk
-include $(THEOS_MAKE_PATH)/tweak.mk
+        [box addSubview:label];
+        [window addSubview:box];
+
+        NSLog(@"[Moustache] dylib injected successfully");
+    });
+}
