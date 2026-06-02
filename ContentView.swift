0a582a5d1ef81fe3f724019d1615738c590353ce
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-02 04:05:48 +0300

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+
+            Text("Moustache 🔥")
+                .foregroundColor(.white)
+                .font(.largeTitle)
+        }
+    }
+}
